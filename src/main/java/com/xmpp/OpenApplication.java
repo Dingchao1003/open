4a3834852c9532b8bd67a1bb0985933c71c4fbae
dingchao <943951714@qq.com>
2018-05-29 17:22:40 +0800

    打包成war，同时支持本地调试

--- src/main/java/com/xmpp/OpenApplication.java
+++ src/main/java/com/xmpp/OpenApplication.java
@@ -3,14 +3,20 @@ package com.xmpp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @MapperScan("com.xmpp.open.mapper")
 @SpringBootApplication
-public class OpenApplication {
+public class OpenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenApplication.class, args);
 	}
+
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(OpenApplication.class);
+//	}
 }
